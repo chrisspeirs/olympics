a8c454a762e240355f510f68abdb06412b0017ce
Chris <speirsy@gmail.com>
2018-01-19 22:19:38 +0000

    final haha

--- olympics/settings.py
+++ olympics/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-
 SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'olympics.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
